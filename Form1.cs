62df2b0a3c66d0563c296934d96a4d2d441c9bcd
koperson <jj670@gmx.net>
2026-05-30 16:21:42 +0200

    MOD add List of all listboxes

--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,18 @@ namespace Calendar
 {
     public partial class Form1 : Form
     {
+        List<ListBox> listBoxes;
         public Form1()
         {
             InitializeComponent();
+            listBoxes = new List<ListBox>();
+            foreach (Control control in base.Controls)
+            {
+                if (control is ListBox)
+                {
+                    listBoxes.Add((ListBox)control);
+                }
+            }
         }
     }
 }
